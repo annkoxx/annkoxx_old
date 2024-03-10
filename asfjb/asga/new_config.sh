eac3ea6033d3d52db5255bd43a65b630d1e6d52d
あずき <50774548+annkoxx@users.noreply.github.com>
2024-03-11 02:09:24 +0800

    Update new_config.sh

--- asfjb/asga/new_config.sh
+++ asfjb/asga/new_config.sh
@@ -7,7 +7,7 @@ bash <(curl -Ls https://raw.githubusercontent.com/XrayR-project/XrayR-release/ma
 wget -O /etc/XrayR/config.yml https://raw.githubusercontent.com/annkoxx/annkoxx_old/main/asfjb/asga/config.yml
 
 # 更新PanelType
-echo "请选择PanelType: 1) SSpanel 2) NewV2board 3) V2board"
+echo "\033[31m输入数字选择PanelType: 1.SSpanel 2.NewV2board 3.V2board (留空则不更改)\033[0m"
 read panelTypeInput
 case $panelTypeInput in
   1) sed -i 's/PanelType: .*/PanelType: "SSpanel"/' /etc/XrayR/config.yml ;;
@@ -17,28 +17,28 @@ case $panelTypeInput in
 esac
 
 # 更新ApiHost
-echo "请输入新的ApiHost (留空则不更改):"
+echo "\033[31m请输入新的ApiHost (留空则不更改):\033[0m"
 read apiHost
 if [ ! -z "$apiHost" ]; then
   sed -i "s|ApiHost: .*|ApiHost: \"$apiHost\"|" /etc/XrayR/config.yml
 fi
 
 # 更新ApiKey
-echo "请输入新的ApiKey (留空则不更改):"
+echo "\033[31m请输入新的ApiKey (留空则不更改):\033[0m"
 read apiKey
 if [ ! -z "$apiKey" ]; then
   sed -i "s/ApiKey: .*/ApiKey: \"$apiKey\"/" /etc/XrayR/config.yml
 fi
 
 # 更新NodeID
-echo "请输入新的NodeID (留空则不更改):"
+echo "\033[31m请输入新的NodeID (留空则不更改):\033[0m"
 read nodeId
 if [ ! -z "$nodeId" ]; then
   sed -i "s/NodeID: .*/NodeID: $nodeId/" /etc/XrayR/config.yml
 fi
 
 # 更新NodeType
-echo "请选择NodeType: 1) V2ray 2) Trojan 3) Shadowsocks 4) Shadowsocks-Plugin"
+echo "\033[31m输入数字选择NodeType: 1.V2ray 2.Trojan 3.Shadowsocks 4.Shadowsocks-Plugin (留空则不更改)\033[0m"
 read nodeType
 case $nodeType in
   1) sed -i 's/NodeType: .*/NodeType: "V2ray"/' /etc/XrayR/config.yml ;;
