236b9e98a9b1698c6d8b1c8837a2e70cb587cd0b
あずき <50774548+annkoxx@users.noreply.github.com>
2025-02-28 20:26:46 +0800

    Update install_script.sh

--- asfjb/jb/awshk/install_script.sh
+++ asfjb/jb/awshk/install_script.sh
@@ -4,40 +4,10 @@ sudo apt update
 sudo apt install expect -y
 
 # A1命令
-echo "正在运行 A1 命令..."
-expect -c '
-spawn bash <(curl -fLSs https://dispatch.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-o -t 777f294a-4c0b-4e10-b375-522b3705f36a -u https://ny.lics.vip"
-expect "请输入服务名 [默认 nyanpass] :"
-send "\r"
-# 处理第二个交互，输入y优化
-expect "是否优化系统参数 [输入 y 优化] :"
-send "y\r"
-# 处理第三个交互，输入y安装常用工具
-expect "是否安装常用工具 [输入 y 安装] :"
-send "y\r"
-# 等待安装过程结束，确保脚本完成
-expect "安装成功"
-send "\r"
-expect eof
-'
+expect -c 'spawn curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh -o nyanpass-install.sh; expect eof; spawn bash nyanpass-install.sh rel_nodeclient "-o -t 56a2a05d-14c9-4d1d-8795-a54ec4a028d2 -u https://ny.321337.xyz"; expect -re "请输入服务名.*" { send "2\r" }; expect -re "是否优化系统参数.*" { send "y\r" }; expect -re "是否安装常用工具.*" { send "y\r" }; expect -re "请输入.*" { send "\r" }; expect eof'
 
 # A2命令
-echo "正在运行 A2 命令..."
-expect -c '
-spawn bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-o -t a7250388-4177-45ff-afbc-afd4f170a6cc -u https://ny.321337.xyz"
-expect "请输入服务名 [默认 nyanpass] :"
-send "1\r"
-# 处理第二个交互，输入y优化
-expect "是否优化系统参数 [输入 y 优化] :"
-send "y\r"
-# 处理第三个交互，输入y安装常用工具
-expect "是否安装常用工具 [输入 y 安装] :"
-send "y\r"
-# 等待安装过程结束，确保脚本完成
-expect "安装成功"
-send "\r"
-expect eof
-'
+expect -c 'spawn curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh -o nyanpass-install.sh; expect eof; spawn bash nyanpass-install.sh rel_nodeclient "-o -t 777f294a-4c0b-4e10-b375-522b3705f36a -u https://ny.lics.vip"; expect -re "请输入服务名.*" { send "\r" }; expect -re "是否优化系统参数.*" { send "y\r" }; expect -re "是否安装常用工具.*" { send "y\r" }; expect -re "请输入.*" { send "\r" }; expect eof'
 
 # 下载并运行 d11.sh 脚本
 echo "下载并运行 d11.sh 脚本..."
