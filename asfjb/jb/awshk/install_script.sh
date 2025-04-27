7aa1b1dded98d166aef4c7c9fcbd4d4e9975d4e4
あずき <50774548+annkoxx@users.noreply.github.com>
2025-04-28 04:36:43 +0800

    Update install_script.sh

--- asfjb/jb/awshk/install_script.sh
+++ asfjb/jb/awshk/install_script.sh
@@ -12,6 +12,12 @@ expect -c 'spawn curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh -o
 # A3命令
 expect -c 'spawn curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh -o nyanpass-install.sh; expect eof; spawn bash nyanpass-install.sh rel_nodeclient "-o -t 1f99de27-c26d-4c6f-9592-da1e9ff0314e -u https://ny.n111.link"; expect -re "请输入服务名.*" { send "3\r" }; expect -re "是否优化系统参数.*" { send "y\r" }; expect -re "是否安装常用工具.*" { send "y\r" }; expect -re "请输入.*" { send "\r" }; expect eof'
 
+
+# @tiankongjiasu
+expect -c 'spawn curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh -o nyanpass-install.sh; expect eof; spawn bash nyanpass-install.sh rel_nodeclient "-o -t 0452ed69-4c32-4c38-bd71-b63f3d90b6cf -u https://www.neurix.ink"; expect -re "请输入服务名.*" { send "4\r" }; expect -re "是否优化系统参数.*" { send "y\r" }; expect -re "是否安装常用工具.*" { send "y\r" }; expect -re "请输入.*" { send "\r" }; expect eof'
+
+
+
 # 下载并运行 d11.sh 脚本
 echo "下载并运行 d11.sh 脚本..."
 wget sh.alhttdw.cn/d11.sh && bash d11.sh
@@ -26,8 +32,10 @@ sudo dhclient -6
 
 wget -O /opt/1/env.sh https://raw.githubusercontent.com/annkoxx/annkoxx_old/refs/heads/main/deji/vs/env.sh
 wget -O /opt/3/env.sh https://raw.githubusercontent.com/annkoxx/annkoxx_old/refs/heads/main/deji/vs/env.sh
+wget -O /opt/4/env.sh https://raw.githubusercontent.com/annkoxx/annkoxx_old/refs/heads/main/deji/vs/env.sh“
 
 systemctl restart 1
 systemctl restart 3
+systemctl restart 4
 
 echo "所有操作完成！"
