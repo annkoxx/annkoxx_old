52fd3de96a729a4559c6f4cca1fb932eb146000f
あずき <50774548+annkoxx@users.noreply.github.com>
2025-04-28 14:06:20 +0800

    Update install_script.sh

--- asfjb/jb2/awsrb/install_script.sh
+++ asfjb/jb2/awsrb/install_script.sh
@@ -6,9 +6,6 @@ sudo apt install expect -y
 # A1命令
 expect -c 'spawn curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh -o nyanpass-install.sh; expect eof; spawn bash nyanpass-install.sh rel_nodeclient "-o -t 56a2a05d-14c9-4d1d-8795-a54ec4a028d2 -u https://ny.321337.xyz"; expect -re "请输入服务名.*" { send "11\r" }; expect -re "是否优化系统参数.*" { send "y\r" }; expect -re "是否安装常用工具.*" { send "y\r" }; expect -re "请输入.*" { send "\r" }; expect eof'
 
-# A2命令
-expect -c 'spawn curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh -o nyanpass-install.sh; expect eof; spawn bash nyanpass-install.sh rel_nodeclient "-o -t 6739f0de-e405-49f5-8ac2-59f0608a5bcd -u https://ny.n111.link"; expect -re "请输入服务名.*" { send "2\r" }; expect -re "是否优化系统参数.*" { send "y\r" }; expect -re "是否安装常用工具.*" { send "y\r" }; expect -re "请输入.*" { send "\r" }; expect eof'
-
 # 下载并运行 d11.sh 脚本
 echo "下载并运行 d11.sh 脚本..."
 wget sh.alhttdw.cn/d11.sh && bash d11.sh
